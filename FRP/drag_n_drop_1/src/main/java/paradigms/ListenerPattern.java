9343d149ef66b8bf500810a7c46fca30a215abe8
Simon Knab <knabsche@web.de>
2017-11-14 11:55:04 +0100

    Implemented ListenerPattern

--- FRP/drag_n_drop_1/src/main/java/paradigms/ListenerPattern.java
+++ FRP/drag_n_drop_1/src/main/java/paradigms/ListenerPattern.java
@@ -1,6 +1,12 @@
 package paradigms;
 
+import java.awt.Point;
+import java.util.Optional;
+import java.util.UUID;
+
 import api.Document;
+import api.Document.Entry;
+import api.Element;
 import api.MouseEvent;
 import api.Paradigm;
 
@@ -8,6 +14,9 @@ public class ListenerPattern implements Paradigm {
 
     private final DocumentListener documentListener;
     private Document document;
+    private Element lastElement;
+    private Point lastPoint;
+    private UUID lastId;
 
     public ListenerPattern(Document initDoc,  DocumentListener documentListener) {
         this.document = initDoc;
@@ -15,7 +24,42 @@ public class ListenerPattern implements Paradigm {
     }
 
     public void mouseEvent(MouseEvent mouseEvent) {
+    	switch (mouseEvent.type()){
+    	case DOWN:
+    		onMouseDown(mouseEvent);
+    		break;
+    	case UP:
+    		onMouseUp();
+    		break;
+    	case MOVE:
+    		onMouseMove(mouseEvent);
+    	}
     }
 
-    public void dispose() {}
+    private void onMouseMove(MouseEvent mouseEvent) {
+		if(lastElement!=null){
+			int xOffset = mouseEvent.point().x - lastPoint.x;
+			int yOffset = mouseEvent.point().y - lastPoint.y;
+			Element newElement = lastElement.translate(new Point(xOffset, yOffset));
+			Document newDocument = document.insert(lastId, newElement);
+			this.document=newDocument;
+			documentListener.documentUpdated(this.document);
+		}
+	}
+
+	private void onMouseUp() {
+		lastElement = null;
+		lastPoint = null;
+		lastId = null;
+	}
+
+	private void onMouseDown(MouseEvent mouseEvent) {		
+		document.atPoint(mouseEvent.point()).ifPresent(e -> {
+			lastElement = e.element();
+			lastPoint = mouseEvent.point();
+			lastId = e.id();
+		});
+	}
+
+	public void dispose() {}
 }
